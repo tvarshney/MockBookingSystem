e295fb35e8021624e4a5a2d1935dd735e9dbd317
KristinaShh <kshalkoska@gmail.com>
2020-05-24 17:04:00 +0200

    Initial commit

---
+++ MockBookingSystem.API/Controllers/MockBookingController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MockBookingSystem.API.Controllers
+{
+    [ApiController]
+    [Route("api/mockbooking")]
+    public class MockBookingController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult GetFlights()
+        {
+            return NotFound();
+        }
+
+        [HttpGet]
+        public ActionResult GetHotels()
+        {
+            return NotFound();
+        }
+    }
+}
