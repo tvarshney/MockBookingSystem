797632a6f71152653269c645955c7ed196055371
KristinaShh <kshalkoska@gmail.com>
2020-07-06 18:43:08 +0200

    Adding Logic to Search Endpoint

---
+++ MockBookingSystem.API/Enums/SearchType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MockBookingSystem.API.Enums
+{
+    public enum SearchType
+    {
+        HotelOnly = 0,
+        HotelAndFlight = 1,
+        LastMinuteHotels =2
+    }
+}
